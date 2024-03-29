9928e5a0cc8927f0d1f13da289445e4f98a63273
PC <PC@DESKTOP-6C8U8SA>
2024-03-28 21:30:58 -0500

    Agregar archivos de proyecto.

---
+++ SUMA_OPERACION/Form1.Designer.cs
@@ -0,0 +1,165 @@
+﻿namespace SUMA_OPERACION
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtPrimerNum = new System.Windows.Forms.TextBox();
+            this.txtSegundoNum = new System.Windows.Forms.TextBox();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.btnSumar = new System.Windows.Forms.Button();
+            this.lblVadiartxt1 = new System.Windows.Forms.Label();
+            this.lblValidarTxt2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(71, 129);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(206, 18);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Digite  El  Primer  Numero";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(71, 195);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(222, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Digite  El  Segundo  Numero";
+            // 
+            // txtPrimerNum
+            // 
+            this.txtPrimerNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtPrimerNum.Location = new System.Drawing.Point(306, 113);
+            this.txtPrimerNum.Multiline = true;
+            this.txtPrimerNum.Name = "txtPrimerNum";
+            this.txtPrimerNum.Size = new System.Drawing.Size(184, 45);
+            this.txtPrimerNum.TabIndex = 2;
+            // 
+            // txtSegundoNum
+            // 
+            this.txtSegundoNum.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtSegundoNum.Location = new System.Drawing.Point(306, 180);
+            this.txtSegundoNum.Multiline = true;
+            this.txtSegundoNum.Name = "txtSegundoNum";
+            this.txtSegundoNum.Size = new System.Drawing.Size(184, 48);
+            this.txtSegundoNum.TabIndex = 3;
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(334, 268);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(327, 55);
+            this.lblResultado.TabIndex = 4;
+            this.lblResultado.Text = "RESULTADO";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(256, 41);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(265, 29);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "OPERACION    SUMA";
+            // 
+            // btnSumar
+            // 
+            this.btnSumar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSumar.Location = new System.Drawing.Point(141, 268);
+            this.btnSumar.Name = "btnSumar";
+            this.btnSumar.Size = new System.Drawing.Size(93, 43);
+            this.btnSumar.TabIndex = 6;
+            this.btnSumar.Text = "SUMAR";
+            this.btnSumar.UseVisualStyleBackColor = true;
+            this.btnSumar.Click += new System.EventHandler(this.btnSumar_Click);
+            // 
+            // lblVadiartxt1
+            // 
+            this.lblVadiartxt1.AutoSize = true;
+            this.lblVadiartxt1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblVadiartxt1.Location = new System.Drawing.Point(514, 129);
+            this.lblVadiartxt1.Name = "lblVadiartxt1";
+            this.lblVadiartxt1.Size = new System.Drawing.Size(57, 18);
+            this.lblVadiartxt1.TabIndex = 7;
+            this.lblVadiartxt1.Text = "validar";
+            // 
+            // lblValidarTxt2
+            // 
+            this.lblValidarTxt2.AutoSize = true;
+            this.lblValidarTxt2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblValidarTxt2.Location = new System.Drawing.Point(514, 195);
+            this.lblValidarTxt2.Name = "lblValidarTxt2";
+            this.lblValidarTxt2.Size = new System.Drawing.Size(57, 18);
+            this.lblValidarTxt2.TabIndex = 8;
+            this.lblValidarTxt2.Text = "validar";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(897, 402);
+            this.Controls.Add(this.lblValidarTxt2);
+            this.Controls.Add(this.lblVadiartxt1);
+            this.Controls.Add(this.btnSumar);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.lblResultado);
+            this.Controls.Add(this.txtSegundoNum);
+            this.Controls.Add(this.txtPrimerNum);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtPrimerNum;
+        private System.Windows.Forms.TextBox txtSegundoNum;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Button btnSumar;
+        private System.Windows.Forms.Label lblVadiartxt1;
+        private System.Windows.Forms.Label lblValidarTxt2;
+    }
+}
+
---
+++ SUMA_OPERACION/Form1.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SUMA_OPERACION
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSumar_Click(object sender, EventArgs e)
+        {
+            bool txt1Es_Numero, txt2Es_Numero;
+            double Num1 = 0, Num2 = 0;
+            
+
+            if (txtPrimerNum.Text == string.Empty || txtSegundoNum.Text == string.Empty)
+            {
+                MessageBox.Show("llene todos los textBox para poder hacer la suma");
+            }
+            if(txtPrimerNum.Text != string.Empty && txtSegundoNum.Text != string.Empty)
+            {
+                txt1Es_Numero = EsNumero(txtPrimerNum.Text);
+                txt2Es_Numero = EsNumero(txtSegundoNum.Text);
+                if (txt1Es_Numero)
+                {
+                    Num1 = Convert.ToDouble(txtPrimerNum.Text);
+                    lblVadiartxt1.Text = "CORRECTO";
+
+                }
+                else
+                {
+                    lblVadiartxt1.Text = "INCORRECTO, NO PUEDE Contener Letras";
+                    txtPrimerNum.Text = "";
+                }
+                if(txt2Es_Numero)
+                {
+                    Num2 = Convert.ToDouble(txtSegundoNum.Text);
+                    lblValidarTxt2.Text = "CORRECTO ";
+                }
+                else
+                {
+                    lblValidarTxt2.Text = "INCORRECTO, NO PUEDE Contener Letras";
+                    txtSegundoNum.Text = "";
+                }
+
+                if (txt1Es_Numero && txt2Es_Numero)
+                {
+                    lblResultado.Text = "" +(Num1 + Num2);//Convert.ToString(Num1+Num2);
+                    Limpiar();
+                }
+            }
+            
+        }
+
+
+        private void Limpiar()
+        {
+            txtPrimerNum.Text = string.Empty;
+            txtSegundoNum.Text = String.Empty;
+        }
+
+        private bool EsNumero(string texto)
+        {
+            bool respuesta;
+
+            respuesta = double.TryParse(texto, out double Num);
+
+            return respuesta;
+        }
+    }
+}
